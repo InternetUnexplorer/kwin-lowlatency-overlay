218cadac78bbb9045063cc87c54416f786e9009d
InternetUnexplorer <internetunexplorer@gmail.com>
2021-01-12 16:25:28 -0800

    Move package to kwin-lowlatency.nix

--- default.nix
+++ default.nix
@@ -1,17 +1,7 @@
 self: super:
 
-let
-  pname = "kwin-lowlatency";
-  version = "5.20.5";
-
-  kwin-lowlatency = super.plasma5.kwin.overrideAttrs (oldAttrs: {
-    name = "${pname}-${version}";
-    patches = (oldAttrs.patches or [ ]) ++ [
-      (super.fetchpatch {
-        url = "https://tildearrow.org/storage/kwin-lowlatency/kwin-lowlatency-${version}.patch";
-        sha256 = "14lzv38kkypqjwgdcd8f7lm7366y7cj08l3lh8qq1b49pza0zb2w";
-      })
-    ];
-  });
-
-in { plasma5 = super.plasma5 // { kwin = kwin-lowlatency; }; }
+{
+  plasma5 = super.plasma5 // {
+    kwin = super.lib.callPackageWith (super) ./kwin-lowlatency.nix { };
+  };
+}
---
+++ kwin-lowlatency.nix
@@ -0,0 +1,14 @@
+{ plasma5, fetchpatch }:
+
+let
+  pname = "kwin-lowlatency";
+  version = "5.20.5";
+in plasma5.kwin.overrideAttrs (oldAttrs: {
+  name = "${pname}-${version}";
+  patches = (oldAttrs.patches or [ ]) ++ [
+    (fetchpatch {
+      url = "https://tildearrow.org/storage/kwin-lowlatency/kwin-lowlatency-${version}.patch";
+      sha256 = "14lzv38kkypqjwgdcd8f7lm7366y7cj08l3lh8qq1b49pza0zb2w";
+    })
+  ];
+})
