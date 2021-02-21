6a9aa1dc011fc31584545011778ae9ef0c4bb6b5
InternetUnexplorer <internetunexplorer@gmail.com>
2021-02-21 15:53:47 -0800

    Remove defaultPackage from flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,4 @@
 {
   description = "NixOS overlay for @tildearrow's kwin-lowlatency";
-
-  outputs = { self, nixpkgs }: {
-    overlay = import ./default.nix;
-
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      callPackage ./kwin-lowlatency.nix { };
-  };
+  outputs = { self }: { overlay = import ./default.nix; };
 }
